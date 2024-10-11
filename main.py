8d236b39b4a53c2a2d85b2be71056e6472c1b2ce
illeath <illeath01@gmail.com>
2024-10-11 11:16:37 +0200

    print frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+def open_books():
+    with open("/home/illeath/workspace/github.com/illeath/bookbot/books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(file_contents)
+def main():
+    open_books()
+
+main()
